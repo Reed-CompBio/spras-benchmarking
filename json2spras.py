c9a10508f6af041fd8d60f6e87c6e66758c23903
gabeah <94339309+gabeah@users.noreply.github.com>
2024-08-06 16:52:22 -0700

    Rough Annotations complete!
    Cytoscape session file is included for reference. Will export images when done making tweaks

--- json2spras.py
+++ json2spras.py
@@ -38,13 +38,18 @@ def create_graphs(RNN_file, SN_file):
         v = get_gene_name(edge["target"])
         
         if u not in RNN:
-            RNN.add_node(u, ensembl = edge["source"])
+            RNN.add_node(u, ensembl = edge["source"], source_set = False, target_set = False)
         if v not in RNN:
-            RNN.add_node(v, ensembl = edge["target"])
+            RNN.add_node(v, ensembl = edge["target"], source_set = False, target_set = False)
         
         flow = float(edge["flow"])
         
         RNN.add_edge(u, v, flow = flow)
+
+    for s_node in nx.all_neighbors(RNN, 'S'):
+        RNN.nodes[s_node]["source_set"] = True
+    for t_node in nx.all_neighbors(RNN, 'T'):
+        RNN.nodes[t_node]["target_set"] = True
         
     with open ("files/benchmark_gamma10.txt", "r") as spras_net:
         for line in spras_net:
@@ -52,17 +57,32 @@ def create_graphs(RNN_file, SN_file):
             tokens = line.strip().split()
             if "Interactor" in tokens:
                 continue
+            
             u = get_gene_name(tokens[0])
             v = get_gene_name(tokens[1])
             
             if u not in SN:
-                SN.add_node(u, ensembl = tokens[0])
+                if u == 'source':
+                    u = 'S'
+                    SN.add_node(u, ensembl = 'S', source_set = False, target_set = False)
+                else:
+                    SN.add_node(u, ensembl = tokens[0], source_set = False, target_set = False)
+                
             if v not in SN:
-                SN.add_node(v, ensembl = tokens[1])
+                if v == 'target':
+                    v = 'T'
+                    SN.add_node(v, ensembl = 'T', source_set = False, target_set = False)
+                else:
+                    SN.add_node(v, ensembl = tokens[1], source_set = False, target_set = False)
             
             flow = float(tokens[2])
             
             SN.add_edge(u, v, flow = flow)
+            
+    for s_node in nx.all_neighbors(SN, 'S'):
+        SN.nodes[s_node]["source_set"] = True
+    for t_node in nx.all_neighbors(SN, 'T'):
+        SN.nodes[t_node]["target_set"] = True
     
     print("Graphs created!") 
     print(f"Graph from YL-lab has {RNN.number_of_nodes()} nodes and {RNN.number_of_edges()} edges")
@@ -95,13 +115,57 @@ def compare_graphs():
         print(f"Creating list of {len(e_intersection)} edges in both edge-sets:")
         print(e_intersection)
         
+    return n_intersection, e_intersection
         
+def write_cytoscape(out_path, n_int, e_int):
+    RNN_eout = "out_path"+"cyto_edge_out_RNN.txt"
+    RNN_nout = "out_path"+"cyto_node_out_RNN.txt"
+    with open(RNN_eout, 'w') as RNN_ef:
+        RNN_ef.write("Interactor1" + '\t' + "Interactor2" + '\t' + "Flows" + "\t" + "Intersection" + "\n")
         
+        for edge in RNN.edges():
+            e_intersection = edge in e_int
+            RNN_ef.write(edge[0] + "\t" + edge[1] + "\t" + str(RNN[edge[0]][edge[1]]['flow']) + '\t' + str(e_intersection) + "\n")
+    
+    with open(RNN_nout, 'w') as RNN_nf:
+        RNN_nf.write("Node Name" + '\t' + "Set type" + '\t' + "Intersection" +'\n')
+        for node in RNN.nodes():
+            n_intersection = node in n_int
+            if RNN.nodes[node]['source_set']:
+                set_type = 1
+            elif RNN.nodes[node]['target_set']:
+                set_type = 2
+            else:
+                set_type = 0
+            RNN_nf.write(node + '\t' + str(set_type) +'\t'+str(n_intersection)+ '\n')
+    
+    SN_eout = "out_path"+"cyto_edge_out_SN.txt"
+    SN_nout = "out_path"+"cyto_node_out_SN.txt"
+    with open(SN_eout, 'w') as SN_ef:
+        SN_ef.write("Interactor1" + '\t' + "Interactor2" + '\t' + "Flows" + "\t" + "Intersection" + "\n")
+        
+        for edge in SN.edges():
+            e_intersection = edge in e_int
+            SN_ef.write(edge[0] + "\t" + edge[1] + "\t" + str(SN[edge[0]][edge[1]]['flow']) + '\t' + str(e_intersection) + "\n")
+    
+    with open(SN_nout, 'w') as SN_nf:
+        SN_nf.write("Node Name" + '\t' + "Set type" + '\t' + "Intersection" +'\n')
+        for node in SN.nodes():
+            n_intersection = node in n_int
+            if SN.nodes[node]['source_set']:
+                set_type = 1
+            elif SN.nodes[node]['target_set']:
+                set_type = 2
+            else:
+                set_type = 0
+            SN_nf.write(node + '\t' + str(set_type) +'\t'+str(n_intersection)+ '\n')
+
         
     
 def main():
     create_graphs("foo", "bar")
-    compare_graphs()
+    n_int, e_int = compare_graphs()
+    write_cytoscape("output/out", n_int, e_int)
     
 if __name__ == "__main__":
     main()
