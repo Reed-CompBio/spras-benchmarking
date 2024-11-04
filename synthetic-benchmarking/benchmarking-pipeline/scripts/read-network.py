8dd5fb221e52c5f5b9d2f0735cb97055f3dbbae7
ctrlaltaf <altafayyubibarelvi@gmail.com>
2024-11-04 13:42:04 -0800

    initial snakemake workflow

---
+++ synthetic-benchmarking/benchmarking-pipeline/scripts/read-network.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from matplotlib import pyplot as plt
+import pandas as pd
+import networkx as nx
+
+# spras_path = Path(__file__).parent.parent / "networks/testing/spras.txt"
+# panther_path = Path(__file__).parent.parent / "networks/testing/panther.txt"
+spras_path = snakemake.input[0]
+panther_path = snakemake.input[1]
+output_path = snakemake.output[0]
+
+spras_df = pd.read_csv(spras_path, sep="\t")
+panther_df = pd.read_csv(panther_path, sep="\t")
+
+G = nx.from_pandas_edgelist(spras_df, source="Node1", target="Node2")
+H = nx.from_pandas_edgelist(panther_df, source="Node1", target="Node2")
+
+plt.figure(figsize=(10, 8))
+pos = nx.spring_layout(G)
+
+nx.draw(
+    G,
+    pos,
+    with_labels=True,
+    node_color="lightblue",
+    node_size=700,
+    font_size=10,
+    font_color="black",
+    edge_color="gray",
+)
+
+plt.title("SPRAS")
+plt.savefig(output_path, format="png", dpi=300)
+plt.close()
