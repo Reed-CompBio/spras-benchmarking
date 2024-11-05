65bf277321c8c1790e894bc463288eccd2b8e1e3
ctrlaltaf <altafayyubibarelvi@gmail.com>
2024-11-05 13:24:43 -0800

    refactored code

--- synthetic-benchmarking/benchmarking-pipeline/scripts/generate_stats.py
+++ synthetic-benchmarking/benchmarking-pipeline/scripts/generate_stats.py
@@ -18,16 +18,20 @@ H_set = set(H.edges())
 union = G_set | H_set
 intersection = G_set & H_set
 jaccard_index = len(intersection) / len(union)
-
-print("union: ", union)
-print("intersection: ", intersection)
-print("Jaccard Index: ", jaccard_index)
-
-# Calculate Edge Overlap Ratios
 overlap_ratio_set1 = (len(intersection) / len(G_set)) * 100
 overlap_ratio_set2 = (len(intersection) / len(H_set)) * 100
 
+
 f = open(output_path, "w+")
+f.write("Node Statistics\n")
+f.write(f"SPRAS Nodes: {len(G.nodes())}\n")
+f.write(f"Panther Nodes: {len(H.nodes())}\n\n")
+
+f.write("Edge Statistics\n")
+f.write(f"SPRAS Nodes: {len(G.edges())}\n")
+f.write(f"Panther Nodes: {len(H.edges())}\n\n")
+
+f.write("Similarity Statistics\n")
 f.write(f"Jaccard Index: {jaccard_index}\n")
 f.write(f"Edge Overlap Ratio (Panther to SPRAS): {overlap_ratio_set1}%\n")
 f.write(f"Edge Overlap Ratio (SPRAS to Panther):: {overlap_ratio_set2}%\n")
