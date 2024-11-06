a0c39dd6ddb91da038931de778e616c160def26b
ctrlaltaf <altafayyubibarelvi@gmail.com>
2024-11-06 10:09:07 -0800

    added heatmap

---
+++ synthetic-benchmarking/benchmarking-pipeline/scripts/generate_heatmap.py
@@ -0,0 +1,52 @@
+from os import write
+from pathlib import Path
+from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+scores_path = snakemake.input.scores
+output_path = snakemake.output[0]
+
+data = []
+algorithms = []
+pathways = []
+
+for idx, file in enumerate(scores_path):
+    file_name = file.split("/")[-1].split(".")[0]
+    parts = file_name.split("_")
+    if parts[0] not in algorithms:
+        algorithms.append(parts[0])
+    if parts[1] not in pathways:
+        pathways.append(parts[1])
+    with open(file, "r") as f:
+        next(f)
+        for line in f:
+            cols = line.split("\t")
+            data.append((parts[0], parts[1], cols[4]))
+
+jaccard_indices_list = []
+
+for algorithm in algorithms:
+    current = []
+    for pathway in pathways:
+        for entry in data:
+            if entry[0] == algorithm and entry[1] == pathway:
+                current.append(float(entry[2]))
+    jaccard_indices_list.append(current)
+
+jaccard_indices = np.array(jaccard_indices_list)
+
+plt.figure(figsize=(10, 8))
+sns.heatmap(
+    jaccard_indices,
+    annot=True,
+    cmap="viridis",
+    xticklabels=pathways,
+    yticklabels=algorithms,
+)
+
+plt.xlabel("Pathway Files")
+plt.ylabel("Algorithms")
+plt.title("Jaccard Index Heatmap")
+plt.savefig(output_path, format="png", dpi=300)
