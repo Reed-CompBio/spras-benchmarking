7165629e652442b058b6fd7ea54f6ac778bbda31
Oliver Faulkner Anderson <Olliefanderson@gmail.com>
2024-11-08 13:12:57 -0800

    Update R script
    Now can access a list of TFs and Receptors to process a PANTHER pathway into sources and targets.

--- preprocessing/SPRAS_Benchmark_Data_Processing.R
+++ preprocessing/SPRAS_Benchmark_Data_Processing.R
@@ -2,8 +2,9 @@ library(tidyverse)
 library(httr)
 # Set working directory so that pathway_path does not have any parent directories
 # Based on GitHub file structure this should be the specific folder for each PANTHER pathway
-setwd('~/Desktop/GitHub_Repos/spras-benchmarking/preprocessing/Beta3_adrenergic_rec/')
+setwd('~/Desktop/GitHub_Repos/spras-benchmarking/preprocessing/Wnt_signaling/')
 
+# Function for processing PANTHER pathways in Extended SIF format into Node/Edge files for benchmarking inputs
 process_panther_pathway <- function(pathway_path) {
   # Read in PANTHER pathway
   panther_pathway <- read.csv(pathway_path, header = FALSE, fill = TRUE, sep = '\t', stringsAsFactors = FALSE)
@@ -99,17 +100,75 @@ process_panther_pathway <- function(pathway_path) {
   # extract only STRING IDs
   string_edges <- edges %>% select(string1, string2)
   
+  # Get Ensembl IDs from Uniprot IDs
+  ensembl_files = list(
+    ids = uniprot_ids,
+    from = "UniProtKB_AC-ID",
+    to = "Ensembl"
+  )
+  
+  # Make the initial submission
+  r <- POST(url = "https://rest.uniprot.org/idmapping/run", body = ensembl_files, encode = "multipart", accept_json())
+  submission <- content(r, as = "parsed")
+  
+  # When job is ready print the results
+  if (isJobReady(submission[["jobId"]])) {
+    url <- paste("https://rest.uniprot.org/idmapping/details/", submission[["jobId"]], sep = "")
+    r <- GET(url = url, accept_json())
+    details <- content(r, as = "parsed")
+    url <- getResultsURL(details[["redirectURL"]])
+    # Using TSV format see: https://www.uniprot.org/help/api_queries#what-formats-are-available
+    url <- paste(url, "?format=tsv", sep = "")
+    r <- GET(url = url, accept_json())
+    ensembleTable = read.table(text = content(r), sep = "\t", header=TRUE)
+    print(ensembleTable)
+  }
+  
+  # Join STRING IDs back to original dataset
+  nodes <- full_join(nodes, ensembleTable, by = c("uniprot" = "From"))
+  nodes <- rename(nodes, ensembl = To)
+  nodes$ensembl <- sapply(str_split(nodes$ensembl, "\\."), `[`, 1)
+  
+  
   write_tsv(string_edges, paste0("STRING-EDGES-", pathway_path))
   write_tsv(edges_rm, paste0("DEL-EDGES-", pathway_path))
   write_tsv(nodes, paste0("NODES-", pathway_path))
   write_tsv(nodes_rm, paste0("DEL-NODES-", pathway_path))
   
+  # Function for taking a processed Node dataset from process_panther_pathway and checking for TFs
+  process_TFs <- function(nodes, pathway_path) {
+    humanTFs <- read_tsv('../human-interactome/Homo_sapiens_TF.txt')
+    matches <- inner_join(nodes, humanTFs, by = c("ensembl" = "Ensembl"))
+    sources <- matches %>% select(id, uniprot, string, ensembl)
+    
+    write_tsv(sources, paste0("SOURCES-", pathway_path))
+    
+    return(sources)
+  }
+  
+  # Function for taking a processed Node dataset from process_panther_pathway and checking for receptors
+  process_receptors <- function(nodes, pathway_path) {
+    receptors <- read_tsv('../human-interactome/Homo_sapiens_surfaceome.txt')
+    receptors <- receptors %>% 
+      select(`UniProt accession`, `Ensembl gene`, `Membranome Almen main-class`) %>%
+      filter(`Membranome Almen main-class` == "Receptors")
+    matches <- inner_join(nodes, receptors, by = c("uniprot" = "UniProt accession"))
+    targets <- matches %>% select(id, uniprot, string, ensembl)
+    
+    write_tsv(targets, paste0("TARGETS-", pathway_path))
+    
+    return(targets)
+  }
+  
+  targets <- process_receptors(nodes, pathway_path)
+  sources <- process_TFs(nodes, pathway_path)
+  
   # Return all four datasets as a list
-  return(list(edges = string_edges, edges_rm = edges_rm, nodes = nodes, nodes_rm = nodes_rm))
+  return(list(edges = string_edges, edges_rm = edges_rm, nodes = nodes, nodes_rm = nodes_rm, sources = sources, targets = targets))
 }
 
 # Example usage:
-pathway_path <- "Beta3_adrenergic_rec.txt"
+pathway_path <- "Wnt_signaling_pathwa.txt"
 result <- process_panther_pathway(pathway_path)
 
 # Access individual datasets
@@ -117,3 +176,5 @@ edges <- result$edges
 edges_rm <- result$edges_rm
 nodes <- result$nodes
 nodes_rm <- result$nodes_rm
+sources <- result$sources
+targets <- result$targets
