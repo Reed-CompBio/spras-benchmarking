9ee295d270f0008366276e33a47f5bbc66889304
Tristan F. <LeoDog896@hotmail.com>
2025-06-17 21:22:49 +0000

    style: fmt?

--- datasets/rn-muscle-skeletal/process.py
+++ datasets/rn-muscle-skeletal/process.py
@@ -9,8 +9,11 @@ def process():
     shutil.copytree(CURRENT_DIR / 'curated', PROCESSED_DIR, dirs_exist_ok=True)
 
     # TODO: what are the actual last two headers called?
-    data = pandas.read_csv(CURRENT_DIR / 'raw' / 'Muscle_Skeletal-Dec2018.tsv', delimiter='\t', header=None,
-                           names=["Interactome1", "Interactome2", "Type1", "Type2", "InteractionType", "Weight", "Const1", "Const2"])
+    data = pandas.read_csv(CURRENT_DIR / 'raw' / 'Muscle_Skeletal-Dec2018.tsv',
+                           delimiter='\t', header=None,
+                           names=["Interactome1", "Interactome2", "Type1",
+                                  "Type2", "InteractionType", "Weight",
+                                  "Const1", "Const2"])
     data = data.drop(columns=["Type1", "Type2", "InteractionType", "Const1", "Const2"])
     data.insert(3, "Direction", "U")
     data.to_csv(PROCESSED_DIR / 'interactome.tsv', sep='\t', header=False, index=False)
