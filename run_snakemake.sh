4ae14214d6311150fc080817138dae42b78abde4
Tristan F.-R. <pub.tristanf@gmail.com>
2025-06-18 21:11:18 +0000

    fix(run_snakemake): add --cores 1

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -3,5 +3,5 @@
 # in a way that respects the origin directory.
 # Instead, we provide a shell script.
 
-uv tool run snakemake -d datasets/rn-muscle-skeletal -s datasets/rn-muscle-skeletal/Snakefile
-uv tool run snakemake -d datasets/yeast-osmotic-stress -s datasets/yeast-osmotic-stress/Snakefile
+uv tool run snakemake --cores 1 -d datasets/rn-muscle-skeletal -s datasets/rn-muscle-skeletal/Snakefile
+uv tool run snakemake --cores 1 -d datasets/yeast-osmotic-stress -s datasets/yeast-osmotic-stress/Snakefile
