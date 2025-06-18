68ee0117f90bdf57197a1c6d2288fc0b51e917b2
Tristan F.-R. <pub.tristanf@gmail.com>
2025-06-18 21:07:11 +0000

    fix: add snakemake through uv

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -3,5 +3,5 @@
 # in a way that respects the origin directory.
 # Instead, we provide a shell script.
 
-snakemake -d datasets/rn-muscle-skeletal -s datasets/rn-muscle-skeletal/Snakefile
-snakemake -d datasets/yeast-osmotic-stress -s datasets/yeast-osmotic-stress/Snakefile
+uv tool run snakemake -d datasets/rn-muscle-skeletal -s datasets/rn-muscle-skeletal/Snakefile
+uv tool run snakemake -d datasets/yeast-osmotic-stress -s datasets/yeast-osmotic-stress/Snakefile
