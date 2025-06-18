62565ca47eafdf5c348615e769b7d4b607ed4395
Tristan F. <LeoDog896@hotmail.com>
2025-06-18 03:54:00 +0000

    ci: bmp test

--- web/src/lib/commit.ts
+++ web/src/lib/commit.ts
@@ -2,5 +2,5 @@ import { execSync } from 'child_process';
 
 const decoder = new TextDecoder()
 
-export const revision = decoder.decode(execSync('git rev-parse HEAD'));
-export const shortRevision = revision.substring(6);
+export const revision = decoder.decode(execSync('git rev-parse HEAD')).trim();
+export const shortRevision = revision.substring(0, 6);
--- web/src/lib/outputStyle.ts
+++ web/src/lib/outputStyle.ts
@@ -1,4 +1,10 @@
+const dataTypes = [
+    'pra',
+    'dmmm'
+]
+
 interface Output {
+    dataType: string;
     datasetName: string;
     algorithm: string;
     paramsHash: string;
@@ -6,26 +12,54 @@ interface Output {
 
 export function parseOutputString(str: string): Output {
     const components = str.split("-");
+    let dataType;
     let datasetName;
     let algorithm;
     let paramsHash;
 
     if (components.length === 4) {
-        // We ignore -params-
-        [datasetName, algorithm, , paramsHash] = components
+        if (dataTypes.includes(components[0])) {
+            // This is a slug URL (type-...)
+            [dataType, datasetName, algorithm, paramsHash] = components
+        } else {
+            // This is fetched straight from the folder - we ignore -params-
+            [datasetName, algorithm, , paramsHash] = components
+        }
     } else if (components.length === 3) {
         [datasetName, algorithm, paramsHash] = components
     } else {
         throw new Error(`Unexpected length of components in ${components}.`)
     }
 
+
+    // We didn't get a data type in the first passthrough - lets see if the dataset name
+    // starts with a type.
+    if (!dataType) {
+        for (let type of dataTypes) {
+            if (datasetName.startsWith(type)) {
+                dataType = type;
+                datasetName = datasetName.substring(type.length);
+                break;
+            }
+        }
+    }
+
+    if (!dataType) {
+        throw new Error(`Dataset name should begin with a type (one of ${dataTypes})`)
+    }
+
     return {
+        dataType,
         datasetName,
         algorithm,
         paramsHash
     }
 }
 
+export function styleOutput(output: Output): string {
+    return `${output.dataType}-${output.datasetName}-${output.algorithm}-${output.paramsHash}`
+}
+
 export function asFolderName(output: Output): string {
-    return `${output.datasetName}-${output.algorithm}-params-${output.paramsHash}`
+    return `${output.dataType}${output.datasetName}-${output.algorithm}-params-${output.paramsHash}`
 }
