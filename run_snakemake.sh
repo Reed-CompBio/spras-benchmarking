f511d5f0982eba38b5126321fb46b25bd05788f8
Tristan F.-R. <pub.tristanf@gmail.com>
2025-06-18 21:02:55 +0000

    docs(run_snakemake.sh): clarifying comment

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# Snakemake does not support importing and orchestrating multiple Snakefiles.
+# Snakemake does not support importing and orchestrating multiple Snakefiles
+# in a way that respects the origin directory.
 # Instead, we provide a shell script.
 
 snakemake -d datasets/rn-muscle-skeletal -s datasets/rn-muscle-skeletal/Snakefile
