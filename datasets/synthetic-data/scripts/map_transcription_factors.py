751a8f2540f9e7b9fd498c6a6d32e3ccb42f0364
Tristan F. <LeoDog896@hotmail.com>
2026-02-04 05:28:27 +0000

    fix: file extensions and such

--- datasets/synthetic-data/scripts/map_transcription_factors.py
+++ datasets/synthetic-data/scripts/map_transcription_factors.py
@@ -28,7 +28,7 @@ def main():
     tf_df = tf_df.merge(idmapping_selected_df, on="Ensembl", how="inner")
     tf_df = tf_df.explode("UniProtKB-AC")
     tf_df = tf_df.fillna("NA")
-    tf_df.to_csv(interactome_folder / "Homo_sapiens_TF_Uniprot.txt", header=True, sep="\t", index=False)
+    tf_df.to_csv(interactome_folder / "Homo_sapiens_TF_Uniprot.tsv", header=True, sep="\t", index=False)
 
 
 if __name__ == "__main__":
