9d3e1941ca3b9b04c9d29545a0d26a5c2753bcc6
Tristan F. <pub.tristanf@gmail.com>
2026-02-12 01:09:01 +0000

    feat: trimming

--- datasets/synthetic-data/scripts/sampling.py
+++ datasets/synthetic-data/scripts/sampling.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import collections
 from typing import OrderedDict, NamedTuple
 from tools.sample import attempt_sample
+from tools.trim import trim_data_file
 
 current_directory = Path(__file__).parent.resolve()
 
@@ -52,16 +53,17 @@ class SourcesTargets(NamedTuple):
     sources: list[str]
     targets: list[str]
 
+def get_node_data(pathway_name: str) -> pandas.DataFrame:
+    return pandas.read_csv(
+        current_directory / ".." / "processed" / pathway_name / f"{pathway_name}_node_prizes.txt", sep="\t", usecols=["NODEID", "sources", "targets"]
+    )
 
-def sources_and_targets(pathway_name: str) -> SourcesTargets:
+def sources_and_targets(pathway_node_prizes_df: pandas.DataFrame) -> SourcesTargets:
     """
     Returns the sources and targets associated with a particular pathway
     """
-    nodes_df = pandas.read_csv(
-        current_directory / ".." / "processed" / pathway_name / f"{pathway_name}_node_prizes.txt", sep="\t", usecols=["NODEID", "sources", "targets"]
-    )
-    sources: list[str] = list(nodes_df[nodes_df["sources"] is True]["NODEID"])
-    targets: list[str] = list(nodes_df[nodes_df["targets"] is True]["NODEID"])
+    sources: list[str] = list(pathway_node_prizes_df[pathway_node_prizes_df["sources"] is True]["NODEID"])
+    targets: list[str] = list(pathway_node_prizes_df[pathway_node_prizes_df["targets"] is True]["NODEID"])
 
     return SourcesTargets(sources, targets)
 
@@ -82,10 +84,15 @@ def main():
 
     # Get information about the pathway
     pathway_df = read_pathway(pathway_name)
-    sources, targets = sources_and_targets(pathway_name)
+    node_data_df = get_node_data(pathway_name)
+    sources, targets = sources_and_targets(node_data_df)
 
     # TODO: isolate percentage constant (this currently builds up 0%, 10%, ..., 100%)
     for percentage in map(lambda x: (x + 1) / 10, range(10)):
+        output_directory = current_directory / '..' / 'thresholded' / str(percentage) / pathway_name
+        output_interactome = output_directory / 'interactome.txt'
+        output_gold_standard = output_directory / 'gold_standard_edges.txt'
+
         print(f"Sampling with {percentage * 100:.1f}% of edges...")
         attempt_number = 1
         while attempt_sample(
@@ -93,11 +100,15 @@ def main():
                 weight_mapping, interactome_df, sources, targets,
                 # TODO: save attempt number
                 # TODO: save sources & targets using trim.py
-                output_interactome=current_directory / '..' / 'thresholded' / str(percentage) / pathway_name / 'interactome.txt',
-                output_gold_standard=current_directory / '..' / 'thresholded' / str(percentage) / pathway_name / 'gold_standard_edges.txt') is None:
+                output_interactome=output_interactome,
+                output_gold_standard=output_gold_standard) is None:
             attempt_number += 1
             print(f"Attempt number {attempt_number}")
-
+        
+        # We're done sampling:
+        (output_directory / 'attempt-number.txt').write_text(attempt_number)
+        # we need to trim our data file as well.
+        trim_data_file(data_df=node_data_df, gold_standard_df=pathway_df).to_csv(output_directory / 'node_prizes.tsv', sep='\t', index=False)
 
 if __name__ == "__main__":
     main()
