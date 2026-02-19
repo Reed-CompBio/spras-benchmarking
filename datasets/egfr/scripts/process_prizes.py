af335c841ba1d11c41f5a61c7edca23d491703bc
Tristan F.-R. <pub.tristanf@gmail.com>
2026-02-19 04:32:09 +0000

    chore: add dummy column

--- datasets/egfr/scripts/process_prizes.py
+++ datasets/egfr/scripts/process_prizes.py
@@ -9,6 +9,8 @@ def main():
         header=None, names=['NODEID', 'prize']
     )
     prizes['active'] = 'True'
+    prizes['dummy'] = 'True'
+
     prizes.to_csv(egfr_directory / 'processed' / 'prizes-uniprot.txt', index=False, sep='\t')
 
 if __name__ == "__main__":
