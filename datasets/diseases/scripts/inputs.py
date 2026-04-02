173cf7624b5eace33f5dd405462d7e06867aedd4
Anna Ritz <aritz@reed.edu>
2026-04-02 11:41:34 -0700

    forgot to add inputs.p

--- datasets/diseases/scripts/inputs.py
+++ datasets/diseases/scripts/inputs.py
@@ -6,7 +6,12 @@ dir_path = Path(__file__).parent.resolve()
 diseases_path = Path(dir_path, "..")
 (diseases_path / "data").mkdir(exist_ok=True, parents=True)
 
-
+'''
+Generates input node set files based on TIGA trait-gene associations and Disease Ontology
+annotations. 
+TODO: not all of these input node set files are necessary for benchmarking; only those with
+disease validation sets. That is processed in `files.py`. 
+'''
 def main():
     # See fetch.py for information on this file's origin
     tiga = pd.read_csv(diseases_path / "raw" / "tiga_gene-trait_stats.tsv", sep="\t")
@@ -18,6 +23,7 @@ def main():
     human_do = human_do.drop_duplicates(subset="label")
     human_do = human_do[["id", "label"]]
 
+    # Get all TIGA trait-gene associations where the traits correspond to Disease Ontology (DO) terms.
     tiga_do = tiga.merge(human_do, left_on="trait", right_on="label", how="inner", validate="many_to_one")
 
     # Mapping ENSG IDs to ENSP IDs through the STRING aliases file
@@ -28,6 +34,8 @@ def main():
 
     # We can create our TIGA-mapped file through merging on the ENSPs
     tiga_string_df = tiga_do.merge(string_aliases, left_on="ensemblId", right_on="ENSP", how="inner")
+
+    ## Write file to inputs.csv
     tiga_string_df.to_csv(diseases_path / "data" / "inputs.csv", index=False)
 
 
