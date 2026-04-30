1b60dcb02a2746ae6dc6a84c9506191f360611e2
Tristan F.-R. <pub.tristanf@gmail.com>
2026-04-30 17:13:58 +0000

    refactor: use other prize value

--- datasets/egfr/scripts/process_input_nodes.py
+++ datasets/egfr/scripts/process_input_nodes.py
@@ -5,6 +5,11 @@ egfr_directory = Path(__file__).parent.resolve() / ".."
 
 SOURCE = "EGF_HUMAN"
 
+# TODO: Identify a programmatic way to generate this magic value. It has the constraints of:
+# - PRIZE_VALUE > prizes.loc[:, "prize"].max()
+# - not too big
+# and this value experimentally was one that worked, but there are others that may work and would admit a more precise description
+PRIZE_VALUE = 10
 
 def main():
     prizes = pandas.read_csv(egfr_directory / "raw" / "egfr-prizes.txt", sep="\t", header=None, names=["NODEID", "prize"])
@@ -12,7 +17,7 @@ def main():
     prizes["target"] = "True"
     # `SOURCE` is designated as the source node and assigned the highest score to anchor reconstruction at the perturbation.
     prizes = pandas.concat(
-        [prizes, pandas.DataFrame({"NODEID": [SOURCE], "prize": [prizes.loc[:, "prize"].max()], "dummy": ["True"], "source": ["True"]})],
+        [prizes, pandas.DataFrame({"NODEID": [SOURCE], "prize": [PRIZE_VALUE], "dummy": ["True"], "source": ["True"]})],
         ignore_index=True,
     )
     prizes["active"] = "True"
