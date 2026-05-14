ec7435188529b5be091c02b7aa8f5c94a65ab9fe
ntalluri <nehatalluri@live.com>
2026-05-14 13:41:45 -0500

    actually merge branch

--- tools/normalize/trim_input_nodes.py
+++ tools/normalize/trim_input_nodes.py
@@ -1,11 +1,6 @@
 import pandas
 
 
-# def trim_input_nodes_file(interactome_df: pandas.DataFrame, input_nodes_df: pandas.DataFrame) -> pandas.DataFrame:
-#     return input_nodes_df.loc[
-#         input_nodes_df["NODEID"].isin(interactome_df["Interactor1"]) & input_nodes_df["NODEID"].isin(interactome_df["Interactor2"]), :
-#     ]
-
 def trim_input_nodes_file(interactome_df: pandas.DataFrame, input_nodes_df: pandas.DataFrame) -> pandas.DataFrame:
     """
     Keep input nodes that appear anywhere in the interactome.
