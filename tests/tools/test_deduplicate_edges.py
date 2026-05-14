1f0ca12ebbd1e4626ffb92221a397ff8c20ffba5
ntalluri <nehatalluri@live.com>
2026-05-14 10:13:13 -0500

    cleanup

--- tests/tools/test_deduplicate_edges.py
+++ tests/tools/test_deduplicate_edges.py
@@ -8,10 +8,8 @@ def test_deduplicate_edges():
         {"Interactor1": ["a", "b", "e", "d"], "Interactor2": ["b", "a", "d", "e"], "Weight": [1, 2, 3, 4], "Direction": ["U", "U", "D", "D"]}
     )
 
-    print(df)
     assert len(df.index) == 4
+    
     deduplicated_df, _ = deduplicate_edges(df)
-
-    print(deduplicated_df)
     assert len(deduplicated_df.index) == 3
     assert list(deduplicated_df.iloc[0]) == ["d", "e", numpy.int64(4), "D"]
