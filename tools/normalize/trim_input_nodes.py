230a76531e5a7e82a8341d87c7229e4d0497e7d0
ntalluri <nehatalluri@live.com>
2026-05-14 11:56:42 -0500

    update trim_input_nodes.py, it was an & and it needed to be an |

--- tools/normalize/trim_input_nodes.py
+++ tools/normalize/trim_input_nodes.py
@@ -2,6 +2,10 @@ import pandas
 
 
 def trim_input_nodes_file(interactome_df: pandas.DataFrame, input_nodes_df: pandas.DataFrame) -> pandas.DataFrame:
+    """
+    Keep input nodes that appear anywhere in the interactome (as Interactor1 or Interactor2)
+    Nodes absent from the interactome cannot contribute to pathway reconstruction 
+    """
     return input_nodes_df.loc[
-        input_nodes_df["NODEID"].isin(interactome_df["Interactor1"]) & input_nodes_df["NODEID"].isin(interactome_df["Interactor2"]), :
+        input_nodes_df["NODEID"].isin(interactome_df["Interactor1"]) | input_nodes_df["NODEID"].isin(interactome_df["Interactor2"]), :
     ]
