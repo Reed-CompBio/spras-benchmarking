e8a3476ddea268ef92369f1351944abf41b044a2
ntalluri <nehatalluri@live.com>
2026-06-17 10:51:42 -0500

    updated code

--- datasets/panther_signaling_pathways/scripts/preprocess/threshold/gather.py
+++ datasets/panther_signaling_pathways/scripts/preprocess/threshold/gather.py
@@ -19,18 +19,23 @@ import glob
 import pandas
 from concurrent.futures import ThreadPoolExecutor
 
-input_files = glob.glob("processed/*/interactomes/overlap_*.tsv")
-print("gathered")
-print(len(input_files))
-output_file = "processed/interactome-pathway-overlap.tsv"
 
-def read_file(f):
-    return pandas.read_csv(f, sep="\t")
+def main():
+    input_files = glob.glob("processed/*/interactomes/overlap_*.tsv")
+    print("gathered")
+    print(len(input_files))
+    output_file = "processed/interactome-pathway-overlap.tsv"
 
-with ThreadPoolExecutor() as executor:
-    dfs = list(executor.map(read_file, input_files))
-print("dfed")
+    def read_file(f):
+        return pandas.read_csv(f, sep="\t")
 
-pandas.concat(dfs, ignore_index=True).sort_values(by=["pathway", "threshold"]).to_csv(output_file, sep="\t", index=False)
-print("concatted")
-print("outputted")
+    with ThreadPoolExecutor() as executor:
+        dfs = list(executor.map(read_file, input_files))
+    print("dfed")
+
+    pandas.concat(dfs, ignore_index=True).sort_values(by=["pathway", "threshold"]).to_csv(output_file, sep="\t", index=False)
+    print("concatted")
+    print("outputted")
+
+if __name__ == "__main__":
+    main()
